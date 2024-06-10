90923f1cdda435ad58933b3ad15d8bd63a310277
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-06-10 16:33:27 -0300

    add verificação de ganhadores

--- JavaFaculdade/src/jogoDaVelha/jogoDaVelha.java
+++ JavaFaculdade/src/jogoDaVelha/jogoDaVelha.java
@@ -49,26 +49,52 @@ public class jogoDaVelha {
 					System.out.println("Entrada Inválida, informe (1, 2 ou 3)");
 				}
 			}
-			System.out.println();
-
 			linha--;
 			coluna--;
-			
+
 			if (jogoDaVelha[linha][coluna] == 'X' || jogoDaVelha[linha][coluna] == 'O') {
 				System.out.println("Esta posição já foi usada, tente novamente.");
 			} else {
 				jogoDaVelha[linha][coluna] = sinal;
-				rodada ++;
+				rodada++;
 			}
 
 			for (i = 0; i < jogoDaVelha.length; i++) {
 				for (j = 0; j < jogoDaVelha[i].length; j++) {
-						System.out.print(jogoDaVelha[i][j]);
-						if (j < jogoDaVelha[i].length - 1) {
-							System.out.print(" | ");
-						}
+					System.out.print(jogoDaVelha[i][j]);
+					if (j < jogoDaVelha[i].length - 1) {
+						System.out.print(" | ");
+					}
 				}
 				System.out.println();
+			}
+
+			if ((jogoDaVelha[0][0] == 'X' && jogoDaVelha[0][1] == 'X' && jogoDaVelha[0][2] == 'X')
+					|| (jogoDaVelha[1][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[1][2] == 'X')
+					|| (jogoDaVelha[2][0] == 'X' && jogoDaVelha[2][1] == 'X' && jogoDaVelha[2][2] == 'X')
+					|| (jogoDaVelha[0][0] == 'X' && jogoDaVelha[1][0] == 'X' && jogoDaVelha[2][0] == 'X')
+					|| (jogoDaVelha[0][1] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][1] == 'X')
+					|| (jogoDaVelha[0][2] == 'X' && jogoDaVelha[1][2] == 'X' && jogoDaVelha[2][2] == 'X')
+					|| (jogoDaVelha[0][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][2] == 'X')
+					|| (jogoDaVelha[0][2] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][0] == 'X')) {
+
+				encerrarJogo = true;
+				System.out.println("O jogador 1 ganhou");
+
+			} else if ((jogoDaVelha[0][0] == 'O' && jogoDaVelha[0][1] == 'O' && jogoDaVelha[0][2] == 'O')
+					|| (jogoDaVelha[1][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[1][2] == 'O')
+					|| (jogoDaVelha[2][0] == 'O' && jogoDaVelha[2][1] == 'O' && jogoDaVelha[2][2] == 'O')
+					|| (jogoDaVelha[0][0] == 'O' && jogoDaVelha[1][0] == 'O' && jogoDaVelha[2][0] == 'O')
+					|| (jogoDaVelha[0][1] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][1] == 'O')
+					|| (jogoDaVelha[0][2] == 'O' && jogoDaVelha[1][2] == 'O' && jogoDaVelha[2][2] == 'O')
+					|| (jogoDaVelha[0][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][2] == 'O')
+					|| (jogoDaVelha[0][2] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][0] == 'O')) {
+
+				encerrarJogo = true;
+				System.out.println("O jogador 2 ganhou.");
+			} else if (rodada > 9) {
+				encerrarJogo = true;
+				System.out.println("Empate, ninguém ganhou.");
 			}
 		}
 
