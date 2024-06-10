72dc31af7984f16abb78b8bc1c62068f60cf04e9
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-06-10 15:19:37 -0300

    definindo algumas variáveis e ordem de jogadas

--- JavaFaculdade/src/jogoDaVelha/jogoDaVelha.java
+++ JavaFaculdade/src/jogoDaVelha/jogoDaVelha.java
@@ -1,10 +1,33 @@
 package jogoDaVelha;
 
+import java.util.Scanner;
+
 public class jogoDaVelha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		char[][] jogoDaVelha = new char[3][3];
+		boolean encerrarJogo = false;
+		int rodada = 1;
+		char sinal;
+		
+		System.out.println("Vamos começar o jogo!");
+		System.out.println("Jogador 1 = (X)");
+		System.out.println("Jogador 2 = (O)");
+		
+		while (!encerrarJogo) {
+			System.out.println("Rodada " + rodada);
+			if (rodada % 2 == 1) {
+				System.out.println("É a vez do jogador 1.");
+				sinal = 'X';
+			} else {
+				System.out.println("É a vez do jogador 2.");
+				sinal = 'O';
+			}
+		}
+		
+		sc.close();
 	}
 
 }
